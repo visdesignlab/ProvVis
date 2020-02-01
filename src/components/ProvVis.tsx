d5d7f3132051e70d69b2c4a561eb119926590156
Kiran Gadhave <kirangadhave2@gmail.com>
2020-02-01 16:52:03 -0700

    feat: basic

--- src/components/ProvVis.tsx
+++ src/components/ProvVis.tsx
@@ -1,11 +1,84 @@
-import React, { FC } from 'react'
-import { Header } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
+import React, { FC } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import {
+  ProvenanceGraph,
+  NodeID,
+  Nodes,
+  ProvenanceNode,
+  isStateNode
+} from '@visdesignlab/provenance-lib-core';
+import translate from '../Utils/translate';
+import { stratify, hierarchy } from 'd3';
 
-export interface ProvVisProps {}
-
-const ProvVis: FC<ProvVisProps> = () => {
-  return <Header as="h1">Hello, World!</Header>
+interface ProvVisProps {
+  graph: ProvenanceGraph<unknown>;
+  root: NodeID;
+  height?: number;
+  width?: number;
+  current: NodeID;
+  nodeMap: Nodes<unknown>;
 }
 
-export default ProvVis
+const ProvVis: FC<ProvVisProps> = ({
+  nodeMap,
+  width = 500,
+  height = 600,
+  root,
+  current
+}: ProvVisProps) => {
+  const nodes: ProvenanceNode<unknown>[] = [];
+
+  let travellingNode = current;
+
+  while (travellingNode !== root) {
+    const node = nodeMap[travellingNode];
+    nodes.push(node);
+    if (isStateNode(node)) {
+      travellingNode = node.parent;
+    }
+  }
+
+  nodes.push(nodeMap[root]);
+
+  nodes.reverse();
+
+  const nodeList = Object.values(nodeMap).filter(
+    d => d.metadata.createdOn! >= nodeMap[root].metadata.createdOn!
+  );
+
+  const strat = stratify()
+    .id((d: any) => d.id)
+    .parentId((d: any) => {
+      if (d.id === root) return null;
+      return d.parent ? d.parent : null;
+    });
+
+  let rootNode = strat(nodeList);
+
+  let heirarchicalRootNode = hierarchy(rootNode, d => d.children);
+
+  console.log(rootNode, heirarchicalRootNode, root);
+
+  const ht = (nodes.length + 1) * 30;
+
+  return (
+    <div id="prov-vis">
+      <svg height={ht} width={width}>
+        <rect height={ht} width={width} fill="none" stroke="black" />
+        {nodes.map((node, idx) => {
+          return (
+            <text
+              key={node.id}
+              transform={translate(0, 30 * (idx + 1))}
+              style={{ fontWeight: node.id === current ? 'bold' : 'initial' }}
+            >
+              {node.label}
+            </text>
+          );
+        })}
+      </svg>
+    </div>
+  );
+};
+
+export default ProvVis;
--- src/stories/Test.stories.tsx
+++ src/stories/Test.stories.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
-import { ProvVis } from '../provvis'
+import React from 'react';
+import { ProvVis } from '../provvis';
+import { ProvenanceGraph } from '@visdesignlab/provenance-lib-core';
+const prov = require('./provenance.json');
 
-export default { title: 'ProvVis' }
+export default { title: 'ProvVis' };
 
-export const basic = () => <ProvVis />
+const provenance: ProvenanceGraph<unknown> = prov;
+
+const fauxRoot = Object.keys(provenance.nodes).find(d =>
+  provenance.nodes[d].label.includes('initial visible')
+)!;
+
+export const basic = () => (
+  <ProvVis graph={prov} root={fauxRoot} current={provenance.current} nodeMap={provenance.nodes} />
+);
