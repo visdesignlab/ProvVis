7497852f10718853d244f468b53e34f44af01769
Kiran Gadhave <kirangadhave2@gmail.com>
2020-02-05 19:09:49 -0700

    feat(added tree layout): tree layout completed

--- src/components/ProvVis.tsx
+++ src/components/ProvVis.tsx
@@ -7,8 +7,10 @@ import {
   ProvenanceNode,
   isStateNode
 } from '@visdesignlab/provenance-lib-core';
+import { stratify, hierarchy, HierarchyNode, tree } from 'd3';
+import { treeLayout } from '../Utils/TreeLayout';
 import translate from '../Utils/translate';
-import { stratify, hierarchy } from 'd3';
+import { Popup, Card } from 'semantic-ui-react';
 
 interface ProvVisProps {
   graph: ProvenanceGraph<unknown>;
@@ -17,65 +19,102 @@ interface ProvVisProps {
   width?: number;
   current: NodeID;
   nodeMap: Nodes<unknown>;
+  changeCurrent: any;
 }
+
+export type StratifiedMap = { [key: string]: HierarchyNode<ProvenanceNode<unknown>> };
+export type StratifiedList = HierarchyNode<ProvenanceNode<unknown>>[];
 
 const ProvVis: FC<ProvVisProps> = ({
   nodeMap,
-  width = 500,
-  height = 600,
+  width = 1500,
+  height = 2000,
   root,
-  current
+  current,
+  changeCurrent
 }: ProvVisProps) => {
-  const nodes: ProvenanceNode<unknown>[] = [];
-
-  let travellingNode = current;
-
-  while (travellingNode !== root) {
-    const node = nodeMap[travellingNode];
-    nodes.push(node);
-    if (isStateNode(node)) {
-      travellingNode = node.parent;
-    }
-  }
-
-  nodes.push(nodeMap[root]);
-
-  nodes.reverse();
-
   const nodeList = Object.values(nodeMap).filter(
     d => d.metadata.createdOn! >= nodeMap[root].metadata.createdOn!
   );
 
-  const strat = stratify()
-    .id((d: any) => d.id)
-    .parentId((d: any) => {
+  const strat = stratify<ProvenanceNode<unknown>>()
+    .id(d => d.id)
+    .parentId(d => {
       if (d.id === root) return null;
-      return d.parent ? d.parent : null;
+      if (isStateNode(d)) {
+        return d.parent;
+      } else {
+        return null;
+      }
     });
 
-  let rootNode = strat(nodeList);
+  const stratifiedTree = strat(nodeList);
+  const stratifiedList: StratifiedList = stratifiedTree.descendants();
+  const stratifiedMap: StratifiedMap = {};
 
-  let heirarchicalRootNode = hierarchy(rootNode, d => d.children);
+  stratifiedList.forEach(c => (stratifiedMap[c.id!] = c));
+  const currentPath = treeLayout(stratifiedMap, stratifiedList, current, root);
 
-  console.log(rootNode, heirarchicalRootNode, root);
+  const adjustedWidth = width * 0.8;
+  const adjustedHeight = height * 0.8;
 
-  const ht = (nodes.length + 1) * 30;
+  console.log(stratifiedList);
+
+  const links = stratifiedTree.links();
 
   return (
     <div id="prov-vis">
-      <svg height={ht} width={width}>
-        <rect height={ht} width={width} fill="none" stroke="black" />
-        {nodes.map((node, idx) => {
-          return (
-            <text
-              key={node.id}
-              transform={translate(0, 30 * (idx + 1))}
-              style={{ fontWeight: node.id === current ? 'bold' : 'initial' }}
-            >
-              {node.label}
-            </text>
-          );
-        })}
+      <svg height={height} width={width}>
+        <rect height={height} width={width} fill="none" stroke="black" />
+        <g transform={translate(width / 2, (height - adjustedHeight) / 2)}>
+          {stratifiedList.map(d => {
+            const temp: any = d;
+            return (
+              <g key={d.id || ''} transform={translate(-150 * temp.width, 100 * temp.depth)}>
+                <Popup
+                  content={
+                    <div>
+                      <p>Width: {temp.width}</p>
+                      <p>Depth: {temp.depth}</p>
+                      <p>Height: {temp.height}</p>
+                      <p>ID: {temp.id}</p>
+                      <p>Parent: {temp.parent?.id}</p>
+                    </div>
+                  }
+                  trigger={
+                    <g>
+                      <circle
+                        onClick={() => changeCurrent(d.id)}
+                        r="10"
+                        fill={currentPath.includes(d.id!) ? 'red' : 'blue'}
+                        stroke={d.id! === current ? 'black' : 'none'}
+                        strokeWidth={5}
+                      />
+                      <text textAnchor="middle" transform="rotate(-45)translate(0, 25)">
+                        {temp.data.label}
+                      </text>
+                    </g>
+                  }
+                ></Popup>
+              </g>
+            );
+          })}{' '}
+          {links.map((d: any) => {
+            const { source, target } = d;
+
+            return (
+              <g key={`${source.id}${target.id}}`}>
+                <line
+                  x1={-150 * source.width}
+                  x2={-150 * target.width}
+                  y1={100 * source.depth}
+                  y2={100 * target.depth}
+                  stroke="black"
+                />
+              </g>
+            );
+          })}
+        </g>
       </svg>
     </div>
   );
--- src/stories/Test.stories.tsx
+++ src/stories/Test.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProvVis } from '../provvis';
 import { ProvenanceGraph } from '@visdesignlab/provenance-lib-core';
 const prov = require('./provenance.json');
@@ -11,6 +11,19 @@ const fauxRoot = Object.keys(provenance.nodes).find(d =>
   provenance.nodes[d].label.includes('initial visible')
 )!;
 
-export const basic = () => (
-  <ProvVis graph={prov} root={fauxRoot} current={provenance.current} nodeMap={provenance.nodes} />
-);
+// provenance.current = '4bf77c2e-ac50-4b6d-b879-93c722eb67f2';
+// provenance.current = '4bf77c2e-ac50-4b6d-b879-93c722eb67f2';
+// provenance.current = '247f3360-0c79-4ee9-811d-c9e05be285eb';
+
+export const basic = () => {
+  let [current, setCurrent] = useState(provenance.current);
+  return (
+    <ProvVis
+      graph={prov}
+      root={fauxRoot}
+      current={current}
+      nodeMap={provenance.nodes}
+      changeCurrent={setCurrent}
+    />
+  );
+};
